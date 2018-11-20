24aeb19774e0c25ef250efa78fa364aaef114794
AJ ONeal <coolaj86@gmail.com>
2018-11-19 22:12:27 -0700

    v0.7.0: add sec1 output

--- bin/eckles.js
+++ bin/eckles.js
@@ -5,10 +5,28 @@ var fs = require('fs');
 var eckles = require('../index.js');
 
 var infile = process.argv[2];
-//var outfile = process.argv[3];
+var format = process.argv[3];
 
-var keypem = fs.readFileSync(infile, 'ascii');
+var key = fs.readFileSync(infile, 'ascii');
 
-eckles.import({ pem: keypem }).then(function (jwk) {
-  console.log(JSON.stringify(jwk, null, 2));
-});
+try {
+  key = JSON.parse(key);
+} catch(e) {
+  // ignore
+}
+
+if ('string' === typeof key) {
+  eckles.import({ pem: key }).then(function (jwk) {
+    console.log(JSON.stringify(jwk, null, 2));
+  }).catch(function (err) {
+    console.error(err);
+    process.exit(1);
+  });
+} else {
+  eckles.export({ jwk: key, format: format }).then(function (pem) {
+    console.log(pem);
+  }).catch(function (err) {
+    console.error(err);
+    process.exit(2);
+  });
+}
--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+set -e
 
 echo ""
 echo ""
@@ -11,5 +12,17 @@ echo ""
 node bin/eckles.js fixtures/privkey-ec-p384.sec1.pem
 node bin/eckles.js fixtures/privkey-ec-p384.pkcs8.pem
 node bin/eckles.js fixtures/pub-ec-p384.spki.pem
+
+echo ""
+echo ""
+node bin/eckles.js fixtures/privkey-ec-p256.jwk sec1
+node bin/eckles.js fixtures/privkey-ec-p256.jwk pkcs8
+node bin/eckles.js fixtures/pub-ec-p256.jwk spki
+
+echo ""
+echo ""
+node bin/eckles.js fixtures/privkey-ec-p384.jwk sec1
+node bin/eckles.js fixtures/privkey-ec-p384.jwk pkcs8
+node bin/eckles.js fixtures/pub-ec-p384.jwk spki
 
 echo ""
